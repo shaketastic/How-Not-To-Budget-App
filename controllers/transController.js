4615babb07900a318c55af8c691ee9a3fe7acf57
shareekaepps <shareekaepps@icloud.com>
2023-04-18 19:31:26 -0400

    mock data created

---
+++ controllers/transController.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+      name: "mortgage",
+      date: "04/15/2023",
+      amount: 2100,
+      from: loanDepot,
+      category: "shelter",
+    },
+    {
+        name: "phone bill",
+        date: "04/10/2023",
+        amount: 115,
+        from: "T-mobile",
+        category: "mobile",
+      },
+      {
+        name: "car insurance",
+        date: "04/15/2023",
+        amount: 125,
+        from: Geico,
+        category: "transportation",
+      },
+      {
+        name: "groceries",
+        date: "04/18/2023",
+        amount: 180,
+        from: Shoprite,
+        category: "food",
+      },
+      {
+        name: "water, gas, electric",
+        date: "04/20/2023",
+        amount: 230,
+        from: "Central Hudson",
+        category: "utilities",
+      },
+      {
+        name: "portfolio",
+        date: "04/05/2023",
+        amount: 200,
+        from: Robinhood,
+        category: "savings",
+      }
+];
