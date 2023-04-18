7e24ca7a766261acb9b242268df24d121f3603a3
shareekaepps <shareekaepps@icloud.com>
2023-04-18 19:05:05 -0400

    server.js set up

---
+++ app.js
@@ -0,0 +1,27 @@
+// dependencies
+const express = require("express");
+const cors = require("cors");
+
+// config
+const app = express();
+
+// middleware
+app.use(express.json()) // parse incoming JSON
+app.use(cors());
+
+// routes
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the How Not to Budget App! Happy Spending");
+});
+
+app.use("/transactions", transController);
+
+
+// error
+app.get("*", (req, res) => {
+    res.status(404).json({error: "This Page cannot be Found"});
+})
+
+
+module.exports = app;
